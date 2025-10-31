1deee884ee29a2275b213ffd0e97aa5d08753c83
Mike McCracken <michael.e.mmccracken@gmail.com>
2025-10-31 17:18:54 -0400

    added acc sched table

--- generate_assign.py
+++ generate_assign.py
@@ -10,7 +10,7 @@ def round_to_block(s, block_size=4):
 
 # treat inst data as dataframe:
 power_df = pd.read_csv('personpower_2026run_TEST.csv')
-power_df['total'] = power_df['leaders'] + power_df['workers']
+power_df['total'] = power_df['experts'] + power_df['novices']
 # line below applies JLAB's 0.5 weighting (and others if applicable)
 power_df['eff_N'] = (power_df['total'] * power_df['effect_frac']).astype(int)
 calc_T = power_df['eff_N'].sum()
@@ -18,122 +18,147 @@ print('total effective shift takers:', calc_T)
 total_RC_vols = power_df['rc'].sum()
 print('total RC shifts volunteered:', total_RC_vols)
 
-# calculate features of shift schedule
+# set start/end of run
 start_date = dt.datetime(2026, 2, 23)
-# end_date = dt.datetime(2026, 8, 1)
-end_date = dt.datetime(2026, 2, 26)
-print('run start date:', start_date)
-print('run end date:  ',end_date)
+end_date = dt.datetime(2026, 8, 1)
+# end_date = dt.datetime(2026, 2, 26)
+print('run start date: ', start_date)
+print('run end date:   ',end_date)
+
+# make a dictionary of start dates/experiments for the acc_sched table
+acc_sched_dates = {start_date.strftime('%Y-%m-%d'): 'TBD', '2026-05-01': 'TBD2'}
+
+# calculate features of shift schedule
 n_days = (end_date - start_date).days + 1
 print('length of run: ', n_days, 'days')
 if n_days / 4 != int(n_days / 4):
     print('ERROR: length of run not divisble by 4 days')
     exit()
-
-n_leader = n_days * 3
-n_worker = n_days * 3
+    
+n_expert = n_days * 3
+n_novice = n_days * 3
 n_RC = n_days
 n_RC_weeks = n_days / 8
-print('number of leader shifts: ', n_leader)
-print('number of worker shifts: ', n_worker)
+print('number of expert shifts: ', n_expert)
+print('number of novice shifts: ', n_novice)
 print('number of RC days:       ', n_RC)
 print('number of RC weeks:      ', n_RC_weeks)
 if n_RC > total_RC_vols:
     print('ERROR: not enough RCs!')
     exit()
 
-# calculate number of leader and worker shifts for each institution
-#worker shifts:  W*N/T 
-#leader shifts:   (L+R)*N/T  minus the count of RC shifts that they provided 
-power_df['n_worker_alloc'] = n_worker * power_df['eff_N'] / calc_T
-power_df['n_leader_alloc'] = (n_leader + n_RC) * power_df['eff_N'] / calc_T - power_df['rc']
-power_df['n_worker_avail'] = power_df['n_worker_alloc']
-power_df['n_leader_avail'] = power_df['n_leader_alloc']
+# calculate number of expert and novice shifts for each institution
+#novice shifts:  W*N/T 
+#expert shifts:   (L+R)*N/T  minus the count of RC shifts that they provided 
+power_df['n_novice_alloc'] = n_novice * power_df['eff_N'] / calc_T
+power_df['n_expert_alloc'] = (n_expert + n_RC) * power_df['eff_N'] / calc_T - power_df['rc']
+power_df['n_novice_avail'] = power_df['n_novice_alloc']
+power_df['n_expert_avail'] = power_df['n_expert_alloc']
 
 print(power_df)
-print('total calculated leader shifts:', power_df['n_leader_alloc'].sum())
-print('total calculated worker shifts:', power_df['n_worker_alloc'].sum())
+print('total calculated expert shifts:', power_df['n_expert_alloc'].sum())
+print('total calculated novice shifts:', power_df['n_novice_alloc'].sum())
 summary_df = power_df.copy()
 
 # now generate shifts schedule
 iter_date = start_date
 date_list, text_date_list = [], []
-leader_assign = {'owl': [], 'day': [], 'eve': []}
-worker_assign = {'owl': ['JLAB', 'JLAB'], 'day': ['JLAB', 'JLAB'], 'eve': ['JLAB', 'JLAB']}
+expert_assign = {'owl': [], 'day': [], 'eve': []}
+novice_assign = {'owl': ['JLAB', 'JLAB'], 'day': ['JLAB', 'JLAB'], 'eve': ['JLAB', 'JLAB']}
+acc_sched = {'exp': [], 'energy': [], 'acc_day': []}
 
-# create copies of person power df for leader and worker assignments
-leader_alloc_df = power_df.drop(columns=['n_worker_alloc', 'n_worker_avail']).copy()
-worker_alloc_df = power_df.drop(columns=['n_leader_alloc', 'n_leader_avail']).copy()
+# create copies of person power df for expert and novice assignments
+expert_alloc_df = power_df.drop(columns=['n_novice_alloc', 'n_novice_avail']).copy()
+novice_alloc_df = power_df.drop(columns=['n_expert_alloc', 'n_expert_avail']).copy()
 
 #for i in range(n_days):
 while iter_date <= end_date:
     # print(i, iter_date.strftime('%Y-%m-%d'))
     for t in ['owl', 'day', 'eve']:
-        # leaders first
+        # experts first
         # remove insts with zero or fewer shifts remaining
-        leader_alloc_df = leader_alloc_df[leader_alloc_df['n_leader_avail'] > 0]    
-        total_avail = leader_alloc_df['n_leader_avail'].sum()
+        expert_alloc_df = expert_alloc_df[expert_alloc_df['n_expert_avail'] > 0]    
+        total_avail = expert_alloc_df['n_expert_avail'].sum()
         # calculate the sampling weights based on number of available shifts remaining
-        leader_alloc_df['sample_weights'] = leader_alloc_df['n_leader_avail'] / total_avail
-        leader_select_inst = leader_alloc_df.sample(1, weights='sample_weights')['inst'].values[0]
+        expert_alloc_df['sample_weights'] = expert_alloc_df['n_expert_avail'] / total_avail
+        expert_select_inst = expert_alloc_df.sample(1, weights='sample_weights')['inst'].values[0]
         # append the institution to the df for four shifts
-        [leader_assign[t].append(leader_select_inst) for _ in range(4)]
+        [expert_assign[t].append(expert_select_inst) for _ in range(4)]
         # decrement the allocation of the chosen institution
-        leader_alloc_df.loc[leader_alloc_df['inst'] == leader_select_inst, 'n_leader_avail'] -= 4 
+        expert_alloc_df.loc[expert_alloc_df['inst'] == expert_select_inst, 'n_expert_avail'] -= 4 
         
-        # workers next... same steps as above
-        worker_alloc_df = worker_alloc_df[worker_alloc_df['n_worker_avail'] > 0]    
-        total_avail = worker_alloc_df['n_worker_avail'].sum()
-        worker_alloc_df['sample_weights'] = worker_alloc_df['n_worker_avail'] / total_avail
-        worker_select_inst = worker_alloc_df.sample(1, weights='sample_weights')['inst'].values[0]
-        [worker_assign[t].append(worker_select_inst) for _ in range(4)]
-        worker_alloc_df.loc[worker_alloc_df['inst'] == worker_select_inst, 'n_worker_avail'] -= 4 
+        # novices next... same steps as above
+        novice_alloc_df = novice_alloc_df[novice_alloc_df['n_novice_avail'] > 0]    
+        total_avail = novice_alloc_df['n_novice_avail'].sum()
+        novice_alloc_df['sample_weights'] = novice_alloc_df['n_novice_avail'] / total_avail
+        novice_select_inst = novice_alloc_df.sample(1, weights='sample_weights')['inst'].values[0]
+        [novice_assign[t].append(novice_select_inst) for _ in range(4)]
+        novice_alloc_df.loc[novice_alloc_df['inst'] == novice_select_inst, 'n_novice_avail'] -= 4 
     
     # append dates in blocks of four
     for _ in range(4):
         date_list.append(iter_date.strftime('%Y-%m-%d'))
         text_date_list.append(iter_date.strftime('%-d-%b-%Y'))
-        iter_date = iter_date + dt.timedelta(days=1)
 
-# remove last block from worker and replace with two days assigned to JLAB
+        # add info to the acc_sched dictionary
+        exp = ''
+        for d, e in acc_sched_dates.items():
+            if iter_date >= dt.datetime(int(d.split('-')[0]), 
+                                        int(d.split('-')[1]), 
+                                        int(d.split('-')[2])):
+                exp = e
+        acc_sched['exp'].append(exp)
+        acc_sched['acc_day'].append(iter_date.strftime('%A'))
+        acc_sched['energy'].append('TBD')
+
+        # increment the date
+        iter_date = iter_date + dt.timedelta(days=1)
+        
+
+# remove last block from novice and replace with two days assigned to JLAB
 for t in ['owl', 'day', 'eve']:
-    [worker_assign[t].pop() for _ in range(4)]
-    [worker_assign[t].append('JLAB') for _ in range(2)]
+    [novice_assign[t].pop() for _ in range(4)]
+    [novice_assign[t].append('JLAB') for _ in range(2)]
 
 print('Inspect these data frames to see whether any institutions have too many available remaining:')
-print(leader_alloc_df)
-print(worker_alloc_df)
+print(expert_alloc_df)
+print(novice_alloc_df)
 
-leader_df = pd.DataFrame({'shiftdate': date_list, 'shift_date': text_date_list, 
-                          'owl': leader_assign['owl'],
-                          'day': leader_assign['day'],
-                          'eve': leader_assign['eve']})
-worker_df = pd.DataFrame({'shiftdate': date_list, 'shift_date': text_date_list, 
-                          'owl': worker_assign['owl'],
-                          'day': worker_assign['day'],
-                          'eve': worker_assign['eve']})
-# print(leader_df)
-# print(worker_df)
+expert_df = pd.DataFrame({'shiftdate': date_list, 'shift_date': text_date_list, 
+                          'owl': expert_assign['owl'],
+                          'day': expert_assign['day'],
+                          'eve': expert_assign['eve']})
+novice_df = pd.DataFrame({'shiftdate': date_list, 'shift_date': text_date_list, 
+                          'owl': novice_assign['owl'],
+                          'day': novice_assign['day'],
+                          'eve': novice_assign['eve']})
+acc_sched_df = pd.DataFrame({'shiftdate': date_list, 'acc_date': text_date_list, 
+                             'exp': acc_sched['exp'],
+                             'acc_day': acc_sched['acc_day'],
+                             'energy': acc_sched['energy']})
+# print(expert_df)
+# print(novice_df)
 
 # write tentative shift assignments to tsv files for the user to review
-leader_df.to_csv('temp_leader_' + dt.datetime.today().strftime('%Y-%m-%d') + '.csv', index=False)
-worker_df.to_csv('temp_worker_' + dt.datetime.today().strftime('%Y-%m-%d') + '.csv', index=False)
+expert_df.to_csv('temp_expert_' + dt.datetime.today().strftime('%Y-%m-%d') + '.csv', index=False)
+novice_df.to_csv('temp_novice_' + dt.datetime.today().strftime('%Y-%m-%d') + '.csv', index=False)
+acc_sched_df.to_csv('temp_acc_sched_' + dt.datetime.today().strftime('%Y-%m-%d') + '.csv', index=False)
 
 # now let's do some summarizing of the assignments. get a copy of earlier power df
 summary = {}
 for t in ['owl', 'day', 'eve']:
-    summary['leader_' + t] = {}
-    summary['worker_' + t] = {}
+    summary['expert_' + t] = {}
+    summary['novice_' + t] = {}
     for i in summary_df['inst']:
-        summary['leader_' + t][i] = len(leader_df[leader_df[t] == i])
-        summary['worker_' + t][i] = len(worker_df[worker_df[t] == i])
-    summary_df['leader_' + t] = summary_df['inst'].map(summary['leader_' + t])
-    summary_df['worker_' + t] = summary_df['inst'].map(summary['worker_' + t])
+        summary['expert_' + t][i] = len(expert_df[expert_df[t] == i])
+        summary['novice_' + t][i] = len(novice_df[novice_df[t] == i])
+    summary_df['expert_' + t] = summary_df['inst'].map(summary['expert_' + t])
+    summary_df['novice_' + t] = summary_df['inst'].map(summary['novice_' + t])
 
-summary_df['total_leader'] = summary_df['leader_owl'] + summary_df['leader_day'] + summary_df['leader_eve']
-summary_df['total_worker'] = summary_df['worker_owl'] + summary_df['worker_day'] + summary_df['worker_eve']
+summary_df['total_expert'] = summary_df['expert_owl'] + summary_df['expert_day'] + summary_df['expert_eve']
+summary_df['total_novice'] = summary_df['novice_owl'] + summary_df['novice_day'] + summary_df['novice_eve']
 
-summary_df = summary_df.drop(columns=['effect_frac', 'n_worker_avail', 'n_leader_avail'])
+summary_df = summary_df.drop(columns=['effect_frac', 'n_novice_avail', 'n_expert_avail'])
 summary_df.loc['totals'] = summary_df.sum(numeric_only=True)
 
 summary_df.to_csv('summary_' + dt.datetime.today().strftime('%Y-%m-%d') + '.csv')    
--- write_to_db.py
+++ write_to_db.py
@@ -5,48 +5,65 @@ import os
 import csv
 from rich import print
 from rich.progress import track
-from time import sleep
+from getpass import getpass
 
 today_str = dt.datetime.today().strftime('%Y-%m-%d')
-leader_df, worker_df = '', ''
-if os.path.exists('temp_leader_' + today_str + '.csv'):
-    leader_df = pd.read_csv('temp_leader_' + today_str + '.csv')
+expert_df, novice_df = '', ''
+if os.path.exists('temp_expert_' + today_str + '.csv'):
+    expert_df = pd.read_csv('temp_expert_' + today_str + '.csv')
 else:
-    print('Error: tentative leader schedule file not found.')
+    print('Error: tentative expert schedule file not found.')
     exit()
     
-if os.path.exists('temp_worker_' + today_str + '.csv'):
-    worker_df = pd.read_csv('temp_worker_' + today_str + '.csv')
+if os.path.exists('temp_novice_' + today_str + '.csv'):
+    novice_df = pd.read_csv('temp_novice_' + today_str + '.csv')
 else:
-    print('Error: tentative worker schedule file not found.')
+    print('Error: tentative novice schedule file not found.')
     exit()
 
-print('Shift assignments files found:')
-print('\tleader shifts:', 'temp_leader_' + today_str + '.csv')
-print('\tworker shifts:', 'temp_leader_' + today_str + '.csv')
+if os.path.exists('temp_acc_sched_' + today_str + '.csv'):
+    acc_sched_df = pd.read_csv('temp_acc_sched_' + today_str + '.csv')
+else:
+    print('Error: tentative novice schedule file not found.')
+    exit()
 
 print()
-write = input('Do you wish to write these shift assignments to the db [y/N]? ')
+print('Accelerator schedule file found:', '\n\ttemp_acc_sched_' + today_str + '.csv')
+
+print()
+print('Shift assignments files found:')
+print('\texpert shifts:', 'temp_expert_' + today_str + '.csv')
+print('\tnovice shifts:', 'temp_expert_' + today_str + '.csv')
+
+print()
+write = input('Do you wish to write the schedule and shift assignments to the db [y/N]? ')
 if write != 'y':
     exit()
 
 print()
-pwd = input('Enter gluexshifts db password: ')
+pwd = getpass('Enter gluexshifts db password: ')
 
 shifts_db = mysql.connector.connect(host='halldweb', user='gluexshiftbot',
                                     password=pwd, database='gluexshifts')
 
 shifts_cursor = shifts_db.cursor()
 
-for i, l_row in leader_df.iterrows():
+print('Writing accelerator schedule to db...')
+for i, a_row in track(acc_sched_df.iterrows()):
+    scmd = "INSERT INTO acc_sched (acc_date, exp, energy, acc_day, shiftdate) VALUES (%s, %s, %s, %s, %s)"
+    vals = (a_row['acc_date'], a_row['exp'], a_row['energy'], a_row['acc_day'], a_row['shiftdate'])
+    # print(scmd % vals)
+    shifts_cursor.execute(scmd, vals)
+
+for i, e_row in expert_df.iterrows():
     scmd = "INSERT INTO expert (shift_date, owl, day, eve, shiftdate) VALUES (%s, %s, %s, %s, %s)"
-    vals = (l_row['shift_date'], l_row['owl'], l_row['day'], l_row['eve'], l_row['shiftdate'])
+    vals = (e_row['shift_date'], e_row['owl'], e_row['day'], e_row['eve'], e_row['shiftdate'])
     print(scmd % vals)
     shifts_cursor.execute(scmd, vals)
 
-for i, w_row in worker_df.iterrows():
+for i, n_row in novice_df.iterrows():
     scmd = "INSERT INTO novice (shift_date, owl, day, eve, shiftdate) VALUES (%s, %s, %s, %s, %s)"
-    vals = (l_row['shift_date'], l_row['owl'], l_row['day'], l_row['eve'], l_row['shiftdate'])
+    vals = (n_row['shift_date'], n_row['owl'], n_row['day'], n_row['eve'], n_row['shiftdate'])
     print(scmd % vals)
     shifts_cursor.execute(scmd, vals)
 
@@ -56,7 +73,10 @@ commit = input('Do you wish to commit the assignments to the db [y/N]? ')
 if commit != 'y':
     exit()
 
-commit = input('Are you REALLY sure you wish to commit the assignments to the db [y/N]? ')
+print()
+print('Are you REALLY sure you wish to commit the assignments to the db?')
+print('This would take a LOT of time and effort to clean up...')
+commit = input('Commit [y/N]? ')
 if commit != 'y':
     exit()
 
