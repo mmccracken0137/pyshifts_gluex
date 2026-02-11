335d5f3b03f783a5aee22ce6f20191e3e854570e
Michael McCracken <michael.e.mccracken@gmail.com>
2026-02-11 15:11:28 -0500

    read from db is completed

--- read_from_db.py
+++ read_from_db.py
@@ -8,80 +8,74 @@ from rich.progress import track
 from getpass import getpass
 from time import sleep
 
-today_str = dt.datetime.today().strftime('%Y-%m-%d')
-expert_df, novice_df = '', ''
-if os.path.exists('temp_expert_' + today_str + '.csv'):
-    expert_df = pd.read_csv('temp_expert_' + today_str + '.csv')
-else:
-    print('Error: tentative expert schedule file not found.')
-    exit()
-    
-if os.path.exists('temp_novice_' + today_str + '.csv'):
-    novice_df = pd.read_csv('temp_novice_' + today_str + '.csv')
-else:
-    print('Error: tentative novice schedule file not found.')
-    exit()
 
-if os.path.exists('temp_acc_sched_' + today_str + '.csv'):
-    acc_sched_df = pd.read_csv('temp_acc_sched_' + today_str + '.csv')
-else:
-    print('Error: tentative novice schedule file not found.')
-    exit()
+t_date = dt.datetime.today().strftime('%Y-%m-%d')
 
 print()
-print('Accelerator schedule file found:', '\n\ttemp_acc_sched_' + today_str + '.csv')
+s_date = input('Enter start date in YYYY-MM-DD format: ')
+e_date = input('Enter end date in YYYY-MM-DD format:   ')
+# s_date = '2025-08-01'
+# e_date = '2025-08-03'
 
-print()
-print('Shift assignments files found:')
-print('\texpert shifts:', 'temp_expert_' + today_str + '.csv')
-print('\tnovice shifts:', 'temp_expert_' + today_str + '.csv')
+start_date = dt.datetime.strptime(s_date, '%Y-%m-%d')
+end_date = dt.datetime.strptime(e_date, '%Y-%m-%d')
+n_days = (end_date - start_date).days
 
-print()
-write = input('Do you wish to write the schedule and shift assignments to the db [y/N]? ')
-if write != 'y':
-    exit()
+print('Reading shifts schedule entries for', start_date.strftime('%Y-%m-%d'),
+      'to', end_date.strftime('%Y-%m-%d'), f'({n_days} days)...')
 
+
+# connect to db
 print()
 pwd = getpass('Enter gluexshifts db password: ')
-
 shifts_db = mysql.connector.connect(host='halldweb', user='gluexshiftbot',
                                     password=pwd, database='gluexshifts')
-
 shifts_cursor = shifts_db.cursor()
 
-print('Writing accelerator schedule to db...')
-for i, a_row in track(acc_sched_df.iterrows()):
-    scmd = "INSERT INTO acc_sched (acc_date, exp, energy, acc_day, shiftdate) VALUES (%s, %s, %s, %s, %s)"
-    vals = (a_row['acc_date'], a_row['exp'], a_row['energy'], a_row['acc_day'], a_row['shiftdate'])
-    # print(scmd % vals)
-    shifts_cursor.execute(scmd, vals)
-    sleep(0.1)
+iter_date = start_date # datetime objects are immutable :^)
+print('Reading accelerator, expert, and novice schedules from db...')
 
-for i, e_row in track(expert_df.iterrows()):
-    scmd = "INSERT INTO expert (shift_date, owl, day, eve, shiftdate) VALUES (%s, %s, %s, %s, %s)"
-    vals = (e_row['shift_date'], e_row['owl'], e_row['day'], e_row['eve'], e_row['shiftdate'])
-    # print(scmd % vals)
-    shifts_cursor.execute(scmd, vals)
-    sleep(0.1)
+# creat output files
+acc_out_file = open(f'./temp_files/read_acc_{s_date}_{e_date}_{t_date}.csv', 'w')
+acc_out_file.write('acc_date,exp,energy,acc_day,shiftdate\n')
+exp_out_file = open(f'./temp_files/read_expert_{s_date}_{e_date}_{t_date}.csv', 'w')
+exp_out_file.write('shift_date,owl,day,eve,shiftdate\n')
+nov_out_file = open(f'./temp_files/read_novice_{s_date}_{e_date}_{t_date}.csv', 'w')
+nov_out_file.write('shift_date,owl,day,eve,shiftdate\n')
 
-for i, n_row in track(novice_df.iterrows()):
-    scmd = "INSERT INTO novice (shift_date, owl, day, eve, shiftdate) VALUES (%s, %s, %s, %s, %s)"
-    vals = (n_row['shift_date'], n_row['owl'], n_row['day'], n_row['eve'], n_row['shiftdate'])
-    # print(scmd % vals)
-    shifts_cursor.execute(scmd, vals)
+
+while iter_date <= end_date:
+    # accelerator sched
+    scmd = "SELECT acc_date, exp, energy, acc_day, shiftdate FROM acc_sched WHERE shiftdate = %s"
+    print(scmd % iter_date.strftime('%Y-%m-%d'))
+    shifts_cursor.execute(scmd, [iter_date.strftime('%Y-%m-%d')])
+    for (acc_date, exp, energy, acc_day, shiftdate) in shifts_cursor:
+        print(f'{acc_date},{exp},{energy},{acc_day},{shiftdate}')
+        acc_out_file.write(f'{acc_date},{exp},{energy},{acc_day},{shiftdate}\n')
+
+    # expert sched
+    scmd = "SELECT shift_date, owl, day, eve, shiftdate FROM expert WHERE shiftdate = %s"
+    print(scmd % iter_date.strftime('%Y-%m-%d'))
+    shifts_cursor.execute(scmd, [iter_date.strftime('%Y-%m-%d')])
+    for (shift_date, owl, day, eve, shiftdate) in shifts_cursor:
+        print(f'{shift_date},{owl},{day},{eve},{shiftdate}')
+        exp_out_file.write(f'{shift_date},{owl},{day},{eve},{shiftdate}\n')
+
+    # novice sched
+    scmd = "SELECT shift_date, owl, day, eve, shiftdate FROM novice WHERE shiftdate = %s"
+    print(scmd % iter_date.strftime('%Y-%m-%d'))
+    shifts_cursor.execute(scmd, [iter_date.strftime('%Y-%m-%d')])
+    for (shift_date, owl, day, eve, shiftdate) in shifts_cursor:
+        print(f'{shift_date},{owl},{day},{eve},{shiftdate}')
+        nov_out_file.write(f'{shift_date},{owl},{day},{eve},{shiftdate}\n')
+
+
     sleep(0.1)
+    iter_date += dt.timedelta(days=1)
+    
+acc_out_file.close()
+exp_out_file.close()
+nov_out_file.close()
 
 print()
-print('The assignments above have been inserted into the db.')
-commit = input('Do you wish to commit the assignments to the db [y/N]? ')
-if commit != 'y':
-    exit()
 
-print()
-print('Are you REALLY sure you wish to commit the assignments to the db?')
-print('This would take a LOT of time and effort to clean up...')
-commit = input('Commit [y/N]? ')
-if commit != 'y':
-    exit()
-
-# shifts_db.commit()
