6c8207469f8ec2d64a61fbbfa189a43c949b8891
Mike McCracken <michael.e.mmccracken@gmail.com>
2026-02-17 07:06:36 -0500

    small fix to check

--- check_shift_rules.py
+++ check_shift_rules.py
@@ -18,6 +18,28 @@ print()
 s_date = '2026-01-01'
 e_date = '2026-12-25'
 
+def date_from_shift(level, shift, date):
+    s_date = shiftdate.replace(hour=0, minute=0, second=0)
+    e_date = shiftdate.replace(hour=8, minute=0, second=0)
+    if level == 'exp':
+        if shift = 'day': 
+            s_date += dt.timedelta(hour=8)
+            e_date += dt.timedelta(hour=8)
+        if shift = 'eve': 
+            s_date += dt.timedelta(hour=8)
+            e_date += dt.timedelta(hour=8)
+    elif level == 'nov':
+        s_date += dt.timedelta(hour=4)
+        e_date += dt.timedelta(hour=4)
+        if shift = 'day': 
+            s_date += dt.timedelta(hour=8)
+            e_date += dt.timedelta(hour=8)
+        if shift = 'eve': 
+            s_date += dt.timedelta(hour=8)
+            e_date += dt.timedelta(hour=8)
+    return s_date, e_date
+
+#'''
 start_date = dt.datetime.strptime(s_date, '%Y-%m-%d')
 end_date = dt.datetime.strptime(e_date, '%Y-%m-%d')
 
@@ -47,7 +69,7 @@ while iter_date <= end_date:
     shifts_cursor.execute(scmd, [iter_date.strftime('%Y-%m-%d')])
     for (shift_date, owl, day, eve, shiftdate) in shifts_cursor:
         print(f'{shift_date},{owl},{day},{eve},{shiftdate}')
-        exp_out_file.write(f'{shift_date},{owl},{day},{eve},{shiftdate}')
+        exp_out_file.write(f'{shift_date},{owl},{day},{eve},{shiftdate}\n')
 
     # novice sched
     scmd = "SELECT shift_date, owl, day, eve, shiftdate FROM novice WHERE shiftdate = %s"
@@ -55,13 +77,45 @@ while iter_date <= end_date:
     shifts_cursor.execute(scmd, [iter_date.strftime('%Y-%m-%d')])
     for (shift_date, owl, day, eve, shiftdate) in shifts_cursor:
         print(f'{shift_date},{owl},{day},{eve},{shiftdate}')
-        nov_out_file.write(f'{shift_date},{owl},{day},{eve},{shiftdate}')
+        nov_out_file.write(f'{shift_date},{owl},{day},{eve},{shiftdate}\n')
 
     sleep(0.3)
     iter_date += dt.timedelta(days=1)
     
 exp_out_file.close()
 nov_out_file.close()
+#'''
 
-print()
+'''
+exp_file = open('../check_expert_{s_date}_{e_date}_{t_date}.csv', 'r')
+nov_file = open('../check_novice_{s_date}_{e_date}_{t_date}.csv', 'r')
+exp_lin = exp_file.readline()
+exp_lin = exp_file.readline()
+nov_lin = nov_file.readline()
+nov_lin = nov_file.readline()
 
+shifters = {}
+shift_types = ['owl, 'day', 'eve']
+while len(exp_lin) > 1 and len(nov_lin) > 1:
+    exp_arr = exp_lin.split(',')
+    date = dt.datetime.strptime(exp_arr[-1], '%Y-%m-%d')
+    for i, sh_type in enumerate(shift_types):
+        person = exp_arr[i+1]
+        if person not in shifters.keys():
+            shifters[person] = {'shifts': []}
+        shifters[person]['shifts'].append(date_from_shift('exp', sh_type, date))
+
+    nov_arr = exp_lin.split(',')
+    date = dt.datetime.strptime(nov_arr[-1], '%Y-%m-%d')
+    for i, sh_type in enumerate(shift_types):
+        person = nov_arr[i+1]
+        if person not in shifters.keys():
+            shifters[person] = {'shifts': []}
+        shifters[person]['shifts'].append(date_from_shift('exp', sh_type, date))
+
+    exp_lin = exp_file.readline()
+    nov_lin = nov_file.readline()
+    
+exp_file.close()
+nov_file.close()
+'''
